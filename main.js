b4bdd72f495ff9f2a79a7941d3f511ddfd080cb9
Sturvi <tanzifa.bagirova@gmail.com>
2023-07-27 01:45:07 +0400

    update

--- main.js
+++ main.js
@@ -9,16 +9,19 @@ btn.addEventListener("click", () => {
     let newli = document.createElement("li");
     newli.className = "list__item";
     list.append(newli);
-    let newSpan = document.createElement("span");
+    const newSpan = document.createElement("span");
     newSpan.className = "li__span";
 
     newli.append(newSpan);
-    let newButton = document.createElement("button");
+    const newButton = document.createElement("button");
     newButton.className = "exit";
     newli.append(newButton);
     const icon = document.createElement("i");
     icon.className = "fa-solid fa-xmark";
     newButton.append(icon);
+    const done = document.createElement("i");
+    done.className = "fa-solid fa-check ";
+    newli.prepend(done);
     let text = input.value;
     newSpan.textContent = text;
 
@@ -26,6 +29,10 @@ btn.addEventListener("click", () => {
 
     newButton.addEventListener("click", () => {
       newli.remove();
+    });
+    newli.addEventListener("click", () => {
+      newSpan.classList.toggle("active");
+      done.classList.toggle("done__exit");
     });
   }
 });
